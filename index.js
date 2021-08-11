76e905abf7285c006ce390e3750157eaea8f0951
Trestin <trestinishak@gmail.com>
2021-08-11 12:02:48 -0500

    commit tree 91abad9906f8f4a4e73ef604facf0b776a33a19c

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(record) {
+    const win = record.find(record => record.result === `W`);
+    if(!!win === true) {
+        return win.year;
+    } else {
+        return undefined;
+    }
+}
